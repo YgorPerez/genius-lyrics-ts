66066248e677d6ae7aeaef96167cd6d53e73b059
Ygor Perez de Oliveira <perezygor@gmail.com>
2023-06-30 15:38:50 -0300

    removed cheerio

--- src/utils/extractsLyrics.ts
+++ src/utils/extractsLyrics.ts
@@ -1,26 +1,33 @@
-import { load } from "cheerio";
-
 /**
  * @param {string} url - Genius URL
  */
 async function extractsLyrics(url: string) {
   try {
     let response = await fetch(url);
-    const data = await response.text();
-    const $ = load(data);
-    let lyrics: string = $('div[class="lyrics"]').text().trim();
+    const html = await response.text();
+    const parser = new DOMParser();
+    const htmlDocument = parser.parseFromString(html, "text/html");
+
+    let lyrics = htmlDocument
+      .querySelector('div[class="lyrics"]')
+      ?.textContent?.trim();
+
     if (!lyrics) {
       lyrics = "";
-      $('div[class^="Lyrics__Container"]').each((i, elem) => {
-        if ($(elem).text().length !== 0 && elem) {
-          // @ts-ignore
-          let snippet = $(elem)
-            .html()
-            .replace(/<br>/g, "\n")
-            .replace(/<(?!\s*br\s*\/?)[^>]+>/gi, "");
-          lyrics += $("<textarea/>").html(snippet).text().trim() + "\n\n";
-        }
-      });
+      htmlDocument
+        .querySelectorAll('div[class^="Lyrics__Container"]')
+        .forEach((elem) => {
+          if (elem && elem.textContent?.length !== 0) {
+            let snippet = elem.textContent
+              ?.replace(/<br>/g, "\n")
+              .replace(/<(?!\s*br\s*\/?)[^>]+>/gi, "") as string;
+
+            lyrics +=
+              snippet +
+              document.querySelector("<textarea/>")?.textContent?.trim() +
+              "\n\n";
+          }
+        });
     }
     if (!lyrics) return null;
     return lyrics.trim();
