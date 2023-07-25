b61b7b81eaf817b72c251a5aa6219deb04cf6ac5
Ygor Perez de Oliveira <azaz22155@gmail.com>
2023-07-25 08:03:28 -0300

    fully working

--- src/getLyrics.ts
+++ src/getLyrics.ts
@@ -1,13 +1,24 @@
 import { parse } from "node-html-parser";
-import { getTitle } from "./utils";
 
-export type searchQuery = {
+export const getTitle = (title: string, artist: string) => {
+  return `${title} ${artist}`
+    .toLowerCase()
+    .replace(/ *\([^)]*\) */g, "")
+    .replace(/ *\[[^\]]*]/, "")
+    .replace(/feat.|ft./g, "")
+    .replace(/\s+/g, " ")
+    .trim();
+};
+
+async function getLyrics({
+  title,
+  artist,
+  optimizeQuery,
+}: {
   title: string;
   artist?: string;
   optimizeQuery?: boolean;
-};
-
-async function getLyrics({ title, artist, optimizeQuery }: searchQuery) {
+}) {
   try {
     const searchUrl = "https://www.google.com/search?q=";
     !artist ? (artist = "") : null;
@@ -16,15 +27,20 @@ async function getLyrics({ title, artist, optimizeQuery }: searchQuery) {
 
     const response = await fetch(reqUrl, {
       headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64)",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        "Accept-Language": "en-US, en;q=0.9",
       },
     });
     const html = await response.text();
-    const htmlDocument = parse(html);
-    console.log({ response, html, htmlDocument });
-    let lyrics = htmlDocument
+    const htmlDocument = parse(html, {
+      blockTextElements: {
+        noscript: true,
+      },
+    });
+    const lyrics = htmlDocument
       .querySelector("[data-lyricid]")
-      ?.innerText?.trim();
+      ?.structuredText?.trim();
 
     if (!lyrics) return null;
     return lyrics;
--- src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-export const getTitle = (title: string, artist?: string) => {
-  return `${title} ${artist}`
-    .toLowerCase()
-    .replace(/ *\([^)]*\) */g, "")
-    .replace(/ *\[[^\]]*]/, "")
-    .replace(/feat.|ft./g, "")
-    .replace(/\s+/g, " ")
-    .trim();
-};
