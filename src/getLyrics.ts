06daed03ebec80f5fa123d59fede2d97afba669e
Ygor Perez de Oliveira <azaz22155@gmail.com>
2023-07-25 05:36:08 -0300

    added user agent header

--- src/getLyrics.ts
+++ src/getLyrics.ts
@@ -14,7 +14,12 @@ async function getLyrics({ title, artist, optimizeQuery }: searchQuery) {
     const song = optimizeQuery ? getTitle(title, artist) : `${title} ${artist}`;
     const reqUrl = `${searchUrl}${encodeURIComponent(song + " lyrics")}`;
 
-    const response = await fetch(reqUrl);
+    const response = await fetch(reqUrl, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 6.3; Win64; x64)   AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36 Viewer/96.9.4688.89",
+      },
+    });
     const html = await response.text();
     const htmlDocument = parse(html);
     let lyrics = htmlDocument
